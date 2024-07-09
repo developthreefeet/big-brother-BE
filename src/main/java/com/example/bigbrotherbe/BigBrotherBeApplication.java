211f35988a7a8e72f78fe62ee5828f48676aeef2
TaetaetataE <gogo589@badmintonfriends.co.kr>
2024-07-09 22:45:10 +0900

    Setting : 초기 프로젝트 세팅

---
+++ src/main/java/com/example/bigbrotherbe/BigBrotherBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigbrotherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBrotherBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigBrotherBeApplication.class, args);
+    }
+
+}
