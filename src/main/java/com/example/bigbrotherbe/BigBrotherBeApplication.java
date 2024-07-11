77a807ba0214c4760a1582ef25b406f386e1ed32
dongyeon1031 <ehddus0087@naver.com>
2024-07-11 17:12:06 +0900

    git push test 2

--- src/main/java/com/example/bigbrotherbe/BigBrotherBeApplication.java
+++ src/main/java/com/example/bigbrotherbe/BigBrotherBeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BigBrotherBeApplication {
 
     public static void main(String[] args) {
-        System.out.println("git push test");
         SpringApplication.run(BigBrotherBeApplication.class, args);
     }
 
