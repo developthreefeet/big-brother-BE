997abf63c3f6b84c55cfbb7119266cb91baefa52
TaetaetaE01 <gogo589@naver.com>
2024-08-14 01:00:31 +0900

    [feat] : admin controller 분리

--- src/main/java/com/example/bigbrotherbe/domain/transactions/controller/TransactionsAdminController.java
+++ src/main/java/com/example/bigbrotherbe/domain/transactions/controller/TransactionsAdminController.java
@@ -1,19 +1,14 @@
 package com.example.bigbrotherbe.domain.transactions.controller;
 
-import com.amazonaws.Response;
-import com.example.bigbrotherbe.domain.meetings.dto.request.MeetingsUpdateRequest;
+
 import com.example.bigbrotherbe.domain.transactions.dto.request.TransactionsUpdateRequest;
 import com.example.bigbrotherbe.domain.transactions.dto.response.TransactionsResponse;
 import com.example.bigbrotherbe.domain.transactions.service.TransactionsService;
-import com.example.bigbrotherbe.domain.transactions.service.TransactionsServiceImpl;
 import com.example.bigbrotherbe.global.exception.response.ApiResponse;
-import com.example.bigbrotherbe.global.ocr.dto.OcrDTO;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 
 import java.util.List;
 
@@ -21,10 +16,24 @@ import static com.example.bigbrotherbe.global.exception.enums.SuccessCode.SUCCES
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/transactions")
-public class TransactionsController {
+@RequestMapping("/api/v1/admin/transactions")
+public class TransactionsAdminController {
 
     private final TransactionsService transactionsService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> registerTransaction(@RequestParam("affiliationId") Long affiliationId,
+                                                                 @RequestPart(value = "file") MultipartFile multipartFile) {
+        transactionsService.register(multipartFile, affiliationId);
+        return ResponseEntity.ok(ApiResponse.success(SUCCESS));
+    }
+
+    @PutMapping("/{transactionsId}")
+    public ResponseEntity<ApiResponse<Void>> updateTransactions(@PathVariable("transactionsId") Long transactionsId,
+                                                                @RequestBody TransactionsUpdateRequest transactionsUpdateRequest) {
+        transactionsService.update(transactionsId, transactionsUpdateRequest);
+        return ResponseEntity.ok(ApiResponse.success(SUCCESS));
+    }
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<TransactionsResponse>>> getTransactions(@RequestParam("affiliationId") Long affiliationId,
