2975cacce6f1032076920d60d188efd43c05ed74
0702Yoon <gkstkddbs99@naver.com>
2024-08-16 14:41:27 +0900

    refactor : emailservice안에서 Checker, maker로 역할 분리

--- src/main/java/com/example/bigbrotherbe/global/email/component/MailService.java
+++ src/main/java/com/example/bigbrotherbe/global/email/component/MailService.java
@@ -1,13 +1,12 @@
-package com.example.bigbrotherbe.global.email;
+package com.example.bigbrotherbe.global.email.component;
 
 import com.example.bigbrotherbe.domain.member.entity.EMailVerification;
 import com.example.bigbrotherbe.domain.member.repository.MailRepository;
+import com.example.bigbrotherbe.global.email.entity.Email;
+import com.example.bigbrotherbe.global.email.entity.EmailVerificationResult;
 import com.example.bigbrotherbe.global.exception.BusinessException;
 import com.example.bigbrotherbe.global.exception.enums.ErrorCode;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.time.Duration;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
@@ -23,39 +22,29 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
     private final MailRepository mailRepository;
-
+    private final MailChecker mailChecker;
+    private final MailMaker mailMaker;
     //    @Value("${spring.mail.auth-code-expiration-millis}")
     private final long authCodeExpirationMillis = 1800000;
+
+    @Transactional
     public void sendCodeToEmail(String toEmail) {
-            checkPresentEmail(toEmail);
-            String title = "명지대 big brother 이메일 인증 번호";
-            String authCode = createCode();
-            sendEmail(toEmail, title, authCode);
-            // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
-            saveEmailAuthCode(toEmail, authCode, Duration.ofMillis(this.authCodeExpirationMillis));
+        deleteByEmailAddress(toEmail);
+        Email email =  mailMaker.makeMail(toEmail);
+        SimpleMailMessage emailForm = mailMaker.createEmailForm(email);
+        sendEmail(emailForm);
+        saveEmailAuthCode(toEmail, email.authCode(), Duration.ofMillis(this.authCodeExpirationMillis));
     }
 
-    private void sendEmail(String toEmail, String title, String text) {
-        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+    private void sendEmail(SimpleMailMessage emailForm) {
         try {
             javaMailSender.send(emailForm);
         } catch (RuntimeException e) {
-            log.debug("MailService.sendEmail exception occur toEmail: {}, " +
-                "title: {}, text: {}", toEmail, title, text);
             throw new BusinessException(ErrorCode.UNABLE_TO_SEND_EMAIL);
         }
     }
 
-    private SimpleMailMessage createEmailForm(String toEmail,
-        String title,
-        String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(title);
-        message.setText(text);
 
-        return message;
-    }
     @Transactional
     protected void saveEmailAuthCode(String emailAddress, String authCode, Duration duration) {
         mailRepository.save(
@@ -65,7 +54,8 @@ public class MailService {
         scheduleDeletion(emailAddress,duration);
     }
 
-    private void scheduleDeletion(String emailAddress, Duration duration) {
+    @Transactional
+    public void scheduleDeletion(String emailAddress, Duration duration) {
         new Thread(() -> {
             try {
                 Thread.sleep(duration.toMillis());
@@ -75,7 +65,7 @@ public class MailService {
             }
         }).start();
     }
-
+    @Transactional(readOnly = true)
     public String getAuthCode(String emailAddress) {
        EMailVerification emailVerification =  mailRepository.findByEmailAddress(emailAddress).orElseThrow
            ( ()-> new IllegalArgumentException("이메일 인증 시간이 만료되었습니다."));
@@ -83,33 +73,23 @@ public class MailService {
     }
 
     @Transactional
-    protected void checkPresentEmail(String toEmail) {
+    protected void deleteByEmailAddress(String toEmail) {
         mailRepository.findByEmailAddress(toEmail)
             .ifPresent(mail -> mailRepository.deleteByEmailAddress(toEmail));
     }
 
-    private String createCode() {
-        int lenth = 6;
-        try {
-            Random random = SecureRandom.getInstanceStrong();
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < lenth; i++) {
-                builder.append(random.nextInt(10));
-            }
-            return builder.toString();
-        } catch (NoSuchAlgorithmException e) {
-            log.debug("MemberService.createCode() exception occur");
-            throw new BusinessException(ErrorCode.NO_SUCH_ALGORITHM);
-        }
-    }
+
 
     public EmailVerificationResult verifiedCode(String email, String authCode) {
-        String redisAuthCode = getAuthCode(email);
-        boolean authResult = redisAuthCode.equals(authCode);
+        String saveAuthCode = getAuthCode(email);
+        boolean authResult = authCode.equals(saveAuthCode);
         if (!authResult) {
             throw new BusinessException(ErrorCode.MISMATCH_VERIFIED_CODE);
         }
         return EmailVerificationResult.of(true);
+    }
+    public EmailVerificationResult verificationDuplicateEmail(String email) {
+            return mailChecker.checkDuplicateEmail(email);
     }
 
 }
