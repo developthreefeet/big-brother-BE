8caf8dad85d09a734a4bd37eb8371d506718391d
TaetaetaE01 <gogo589@naver.com>
2024-08-19 19:50:21 +0900

    [refactor] : rule 상세조회 response 수정

--- src/main/java/com/example/bigbrotherbe/domain/rule/dto/response/RuleResponse.java
+++ src/main/java/com/example/bigbrotherbe/domain/rule/dto/response/RuleResponse.java
@@ -8,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -17,6 +19,8 @@ public class RuleResponse {
     private Long ruleId;
     private String title;
     private Long affiliationId;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
     private List<FileResponse> fileInfo;
 
     public static RuleResponse fromRuleResponse(Rule rule, List<FileResponse> fileInfo) {
@@ -24,6 +28,8 @@ public class RuleResponse {
                 .ruleId(rule.getId())
                 .title(rule.getTitle())
                 .affiliationId(rule.getAffiliationId())
+                .createAt(rule.getCreateAt())
+                .updateAt(rule.getUpdateAt())
                 .fileInfo(fileInfo)
                 .build();
     }
