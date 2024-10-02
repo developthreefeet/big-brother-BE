2a626263d794bd3b29c778c8e118aed8ab3c5f61
0702Yoon <gkstkddbs99@naver.com>
2024-10-02 12:28:42 +0900

    feat : member, admin 부분 분리

--- src/main/java/com/example/bigbrotherbe/global/swagger/SwaggerConfig.java
+++ src/main/java/com/example/bigbrotherbe/global/swagger/SwaggerConfig.java
@@ -19,20 +19,36 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("My API")
-                        .version("1.0")
-                        .description("My API Description"))
-            .servers(List.of(
-                new Server().url(DOMAIN_URL)  // HTTPS로 설정
-                    .description("Production server")
-            ))
-            .components(new Components()
-                .addSecuritySchemes("Authorization", new SecurityScheme()
-                    .type(SecurityScheme.Type.APIKEY)
-                    .in(SecurityScheme.In.HEADER)
-                    .name("Authorization")
-                ))
-            .addSecurityItem(new SecurityRequirement().addList("Authorization"));
+                .info(apiInfo())
+            .servers(httpsServer())
+            .components(createComponents())
+            .addSecurityItem(createSecurityRequirement());
+    }
+
+    private static SecurityRequirement createSecurityRequirement() {
+        return new SecurityRequirement().addList("Authorization");
+    }
+
+    private static Components createComponents() {
+        return new Components()
+            .addSecuritySchemes("Authorization", new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+            );
+    }
+
+    private static List<Server> httpsServer() {
+        return List.of(
+            new Server().url(DOMAIN_URL)  // HTTPS로 설정
+                .description("Production server")
+        );
+    }
+
+    private static Info apiInfo() {
+        return new Info().title("My API")
+            .version("1.0")
+            .description("My API Description");
     }
 
     @Bean
@@ -41,6 +57,22 @@ public class SwaggerConfig {
                 .group("전체")
                 .pathsToMatch("/api/v1/**")
                 .build();
+    }
+
+    @Bean
+    public GroupedOpenApi memberApi() {
+        return GroupedOpenApi.builder()
+            .group("멤버 API")
+            .pathsToMatch("/api/v1/members/**") // 멤버 관련 엔드포인트만 포함
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+            .group("어드민 API")
+            .pathsToMatch("/api/v1/admin/**") // 관리자 관련 엔드포인트만 포함
+            .build();
     }
 
 }
